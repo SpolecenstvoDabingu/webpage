4ac09cc56e0e3a0895284a0ed7a8754e4807727e
Makhuta <Makhuta@users.noreply.github.com>
2025-02-20 19:33:00 +0000

    deploy: fde7fcb50f2f94434fcc268675791d4c37b03b1e

---
+++ assets/js/upcomming.js
@@ -0,0 +1,176 @@
+function get_series_with_movies(data) {
+    var result = [];
+
+
+    data.forEach(series => {
+        if(series.upcomming.movies.length > 0) {
+            result.push(series);
+        }
+    });
+
+    return result;
+}
+
+function get_series_with_serials(data) {
+    var result = [];
+
+
+    data.forEach(series => {
+        if(series.upcomming.episodes.length > 0) {
+            result.push(series);
+        }
+    });
+
+    return result;
+}
+
+function get_series_with_scenes(data) {
+    var result = [];
+
+
+    data.forEach(series => {
+        if(series.upcomming.scenes.length > 0) {
+            result.push(series);
+        }
+    });
+
+    return result;
+}
+
+
+function make_movies(data) {
+    var element = document.getElementById("movies");
+
+    element.innerHTML = get_series_with_movies(data).map((series, index) => {
+        if (index % 2 === 0) {
+            return `
+                ${index > 0 ? "</div>" : ""}
+                <div class="row py-2">
+                    <div class="col-md-6 d-flex align-items-center">
+                        <a href="#" class="d-flex w-100 text-decoration-none">
+                            <div class="position-relative flex-shrink-0" style="width: 35%;">
+                                <img src="${series.thumbnail}" alt="${series.name}" class="img-fluid rounded-start" 
+                                    style="object-fit: cover; width: 100%; height: auto;">
+                                <div class="fade-overlay"></div>
+                            </div>
+                            <div class="flex-grow-1 text-end px-3">
+                                <h4 class="fw-bold text-spolecenstvo">${series.name}</h4>
+                                <p class="text-muted mb-0 description text-spolecenstvo">${series.description}</p>
+                            </div>
+                        </a>
+                    </div>
+            `;
+        }
+    
+        return `
+            <div class="col-md-6 d-flex align-items-center">
+                <a href="#" class="d-flex w-100 text-decoration-none">
+                    <div class="position-relative flex-shrink-0" style="width: 35%;">
+                        <img src="${series.thumbnail}" alt="${series.name}" class="img-fluid rounded-start" 
+                            style="object-fit: cover; width: 100%; height: auto;">
+                        <div class="fade-overlay"></div>
+                    </div>
+                    <div class="flex-grow-1 text-end px-3">
+                        <h4 class="fw-bold text-spolecenstvo">${series.name}</h4>
+                        <p class="text-muted mb-0 description text-spolecenstvo">${series.description}</p>
+                    </div>
+                </a>
+            </div>
+        `;
+    }).join('') + (data.length % 2 !== 0 ? "</div>" : "");
+}
+
+
+
+function make_serials(data) {
+    var element = document.getElementById("serials");
+
+    element.innerHTML = get_series_with_serials(data).map((series, index) => {
+
+        if (index % 2 === 0) {
+            return `
+                ${index > 0 ? "</div>" : ""}
+                <div class="row py-2">
+                    <div class="col-md-6 d-flex align-items-center">
+                        <a href="#" class="d-flex w-100 text-decoration-none">
+                            <div class="position-relative flex-shrink-0" style="width: 35%;">
+                                <img src="${series.thumbnail}" alt="${series.name}" class="img-fluid rounded-start" 
+                                    style="object-fit: cover; width: 100%; height: auto;">
+                                <div class="fade-overlay"></div>
+                            </div>
+                            <div class="flex-grow-1 text-end px-3">
+                                <h4 class="fw-bold text-spolecenstvo">${series.name}</h4>
+                                <p class="text-muted mb-0 description text-spolecenstvo">${series.description}</p>
+                            </div>
+                        </a>
+                    </div>
+            `;
+        }
+    
+        return `
+            <div class="col-md-6 d-flex align-items-center">
+                <a href="#" class="d-flex w-100 text-decoration-none">
+                    <div class="position-relative flex-shrink-0" style="width: 35%;">
+                        <img src="${series.thumbnail}" alt="${series.name}" class="img-fluid rounded-start" 
+                            style="object-fit: cover; width: 100%; height: auto;">
+                        <div class="fade-overlay"></div>
+                    </div>
+                    <div class="flex-grow-1 text-end px-3">
+                        <h4 class="fw-bold text-spolecenstvo">${series.name}</h4>
+                        <p class="text-muted mb-0 description text-spolecenstvo">${series.description}</p>
+                    </div>
+                </a>
+            </div>
+        `;
+    }).join('') + (data.length % 2 !== 0 ? "</div>" : "");
+}
+
+
+
+function make_scenes(data) {
+    var element = document.getElementById("scenes");
+
+    element.innerHTML = get_series_with_scenes(data).map((series, index) => {
+        if (index % 2 === 0) {
+            return `
+                ${index > 0 ? "</div>" : ""}
+                <div class="row py-2">
+                    <div class="col-md-6 d-flex align-items-center">
+                        <a href="#" class="d-flex w-100 text-decoration-none">
+                            <div class="position-relative flex-shrink-0" style="width: 35%;">
+                                <img src="${series.thumbnail}" alt="${series.name}" class="img-fluid rounded-start" 
+                                    style="object-fit: cover; width: 100%; height: auto;">
+                                <div class="fade-overlay"></div>
+                            </div>
+                            <div class="flex-grow-1 text-end px-3">
+                                <h4 class="fw-bold text-spolecenstvo">${series.name}</h4>
+                                <p class="text-muted mb-0 description text-spolecenstvo">${series.description}</p>
+                            </div>
+                        </a>
+                    </div>
+            `;
+        }
+    
+        return `
+            <div class="col-md-6 d-flex align-items-center">
+                <a href="#" class="d-flex w-100 text-decoration-none">
+                    <div class="position-relative flex-shrink-0" style="width: 35%;">
+                        <img src="${series.thumbnail}" alt="${series.name}" class="img-fluid rounded-start" 
+                            style="object-fit: cover; width: 100%; height: auto;">
+                        <div class="fade-overlay"></div>
+                    </div>
+                    <div class="flex-grow-1 text-end px-3">
+                        <h4 class="fw-bold text-spolecenstvo">${series.name}</h4>
+                        <p class="text-muted mb-0 description text-spolecenstvo">${series.description}</p>
+                    </div>
+                </a>
+            </div>
+        `;
+    }).join('') + (data.length % 2 !== 0 ? "</div>" : "");
+}
+
+function runMakers(data) {
+    make_movies(data);
+    make_serials(data);
+    make_scenes(data);
+}
